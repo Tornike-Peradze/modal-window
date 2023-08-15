407a3907b4486964c04b200cf0c33ebd705cb041
Tornike-Peradze <t_peradze@cu.edu.ge>
2023-08-15 12:55:50 +0400

    jss functionality starting point

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-'use strict';
+"use strict";
 
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", function () {
+    console.log("Button clicked");
+  });
+}
